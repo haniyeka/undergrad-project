30f25df16c466cd9b00ea4d760ce095e36ea306f
Haniye Kashgarani <60451859+haniyeka@users.noreply.github.com>
2023-07-25 14:16:38 -0600

    Update AdminPanelController.cs

--- Libreria/my pro/Controllers/AdminPanelController.cs
+++ Libreria/my pro/Controllers/AdminPanelController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+// Author: Haniye Kashgarani
+// ORCID: 0000-0001-6059-1920
+// Github: haniyeka
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
